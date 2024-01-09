c27d3635d917e3845a3061f24be34076ae07fe5a
Damon Laptev <dimastao@gmail.com>
2024-01-09 14:53:36 +0300

    first commit

---
+++ theory.js
@@ -0,0 +1,13 @@
+// Primitive data types
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. BigInt
+// 7. Symbol
+
+// Create a variable
+let city = "Odessa"; //string
+
+let country;
