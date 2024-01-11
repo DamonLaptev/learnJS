f987625012af429c82aa08ec67c7fd68cdd61478
Damon Laptev <dimastao@gmail.com>
2024-01-11 18:06:48 +0300

    Solve exercises

---
+++ 3.BooleanOperators/theory.js
@@ -0,0 +1,38 @@
+//strict equality
+console.log(5 === 5);
+console.log(5 === 4);
+
+//strict not equal
+console.log(5 !== 5);
+console.log(4 !== 5);
+
+// boolean "mathematical" operators
+console.log(9 > 8);
+console.log(9 < 2);
+console.log(9 <= 9);
+console.log(9 <= 3);
+console.log(3 >= 3);
+console.log(3 >= 4);
+
+// and or operators
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+//!(not)
+console.log(!true);
+console.log(!false);
+
+// Falsy values
+// false
+// 0
+// ""
+// null
+// undefined
+// NaN
