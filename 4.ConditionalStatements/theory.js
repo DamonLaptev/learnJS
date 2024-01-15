bd0cff7dad93cde4adc068fc9cc2dd6d41daf7eb
Damon Laptev <dimastao@gmail.com>
2024-01-15 19:00:39 +0300

    new lesson

---
+++ 4.ConditionalStatements/theory.js
@@ -0,0 +1,17 @@
+// if
+let points = 10;
+
+if (points > 5) {
+  console.log("You have more then 5 points!");
+}
+
+console.log("All done!");
+
+// if else
+let happy = true;
+
+if (happy === true) {
+  console.log("Yay!");
+} else {
+  console.log("Oh, no!");
+}
