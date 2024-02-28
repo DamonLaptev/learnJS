f4504392b1ae09922a662182bdf4496107b659b3
Damon Laptev <dimastao@gmail.com>
2024-02-28 13:44:44 +0300

    new exercises done!

---
+++ 8.Arrays/exercise-warmup.js
@@ -0,0 +1,26 @@
+/*
+    1. Create an array with the following strings:
+        - "Mercury"
+        - "Venus"
+        - "Earth"
+        - "Mars"
+
+    2. Add the string "Jupiter" to the END of the array
+    3. Add the string "Sun" to the START of the array
+    4. Remove the last two strings from the array
+    5. Remove the first two strings from the array
+
+    *Print out the array after each of these steps ^
+*/
+
+const planet = ["Mercury", "Venus", "Earth", "Mars"];
+planet.push("Jupiter");
+
+planet.shift("Sun");
+
+planet.pop();
+planet.pop();
+
+planet.unshift();
+planet.unshift();
+console.log(planet);
---
+++ 8.Arrays/theory.js
@@ -0,0 +1,15 @@
+// Regular function
+
+function name(params) {
+  // code here
+}
+
+//"Arrow" function, function declaration
+
+const printHello = () => {
+  //code here
+};
+
+// function call
+
+printHello();
